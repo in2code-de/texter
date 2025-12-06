c9ffcbe02fc2bfa2b496d7b10961b3af043a871f
Alexander Kellner <alexander.kellner@in2code.de>
2025-12-06 20:36:13 +0100

    [FEATURE] Tested and released for TYPO3 14

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '13.4.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
